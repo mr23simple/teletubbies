19da8f0a2e990be4bdc47adafdef4a9247e4484c
Tmcplata <55778884+Tmcplata-bot@users.noreply.github.com>
2020-08-21 00:15:37 +0800

    Update admin.php
    follow-up admin fixes

--- views/admin.php
+++ views/admin.php
@@ -112,16 +112,16 @@ $conn = new mysqli($servername, $username, $password, $databasename) or die(mysq
                     {
                         echo'<div style="max-height: 400px; overflow: scroll;">
                         <div style="width: 100%;">
-                        <table class="table table-hover text-center" id="myTable">
+                        <table class="table table-hover text-center" id="myTable1">
                                 <thead class="thead-dark">
                                     <tr>
-                                        <th style="cursor: pointer;" onclick="sortTable(0)">User ID</th>
-                                        <th style="cursor: pointer;" onclick="sortTable(1)">Company Name</th>
-                                        <th style="cursor: pointer;" onclick="sortTable(2)">Email</th>
-                                        <th style="cursor: pointer;" onclick="sortTable(3)">Phone Number</th>
-                                        <th style="cursor: pointer;" onclick="sortTable(4)">Address</th>
-                                        <th style="cursor: pointer;" onclick="sortTable(5)">User Type</th>
-                                        <th style="cursor: pointer;" onclick="sortTable(6)">Corporate</th>
+                                        <th style="cursor: pointer;" onclick="sortTable1(0)">User ID</th>
+                                        <th style="cursor: pointer;" onclick="sortTable1(1)">Company Name</th>
+                                        <th style="cursor: pointer;" onclick="sortTable1(2)">Email</th>
+                                        <th style="cursor: pointer;" onclick="sortTable1(3)">Phone Number</th>
+                                        <th style="cursor: pointer;" onclick="sortTable1(4)">Address</th>
+                                        <th style="cursor: pointer;" onclick="sortTable1(5)">User Type</th>
+                                        <th style="cursor: pointer;" onclick="sortTable1(6)">Corporate</th>
                                     </tr>
                             </thead>
                             <tbody>';
@@ -157,6 +157,11 @@ $conn = new mysqli($servername, $username, $password, $databasename) or die(mysq
               </form>
             </div>
 
+            <!-- Divider -->
+            <div class="divider  mb-5">
+                <hr>
+            </div>
+
             <!-- check if a checkbox is checked -->
             <script>
                 function checkboxes()
@@ -178,6 +183,73 @@ $conn = new mysqli($servername, $username, $password, $databasename) or die(mysq
                 {
                     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
                     table = document.getElementById("myTable");
+                    switching = true;
+                    //Set the sorting direction to ascending:
+                    dir = "asc"; 
+                    /*Make a loop that will continue until
+                    no switching has been done:*/
+                    while (switching) 
+                    {
+                        //start by saying: no switching is done:
+                        switching = false;
+                        rows = table.rows;
+                        /*Loop through all table rows (except the
+                        first, which contains table headers):*/
+                        for (i = 1; i < (rows.length - 1); i++) 
+                        {
+                            //start by saying there should be no switching:
+                            shouldSwitch = false;
+                            /*Get the two elements you want to compare,
+                            one from current row and one from the next:*/
+                            x = rows[i].getElementsByTagName("TD")[n];
+                            y = rows[i + 1].getElementsByTagName("TD")[n];
+                            /*check if the two rows should switch place,
+                            based on the direction, asc or desc:*/
+                            if (dir == "asc") {
+                                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                                //if so, mark as a switch and break the loop:
+                                shouldSwitch= true;
+                                break;
+                                }
+                            } 
+                            else if (dir == "desc") 
+                            {
+                                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) 
+                                {
+                                //if so, mark as a switch and break the loop:
+                                shouldSwitch = true;
+                                break;
+                                }
+                            }
+                        }
+                        if (shouldSwitch) 
+                        {
+                        /*If a switch has been marked, make the switch
+                        and mark that a switch has been done:*/
+                        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                        switching = true;
+                        //Each time a switch is done, increase this count by 1:
+                        switchcount ++;      
+                        } else 
+                        {
+                            /*If no switching has been done AND the direction is "asc",
+                            set the direction to "desc" and run the while loop again.*/
+                            if (switchcount == 0 && dir == "asc") 
+                            {
+                                dir = "desc";
+                                switching = true;
+                            }
+                        }
+                    }
+                }
+            </script>
+
+            <!-- sort table contents on click -->
+            <script>
+                function sortTable1(n) 
+                {
+                    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+                    table = document.getElementById("myTable1");
                     switching = true;
                     //Set the sorting direction to ascending:
                     dir = "asc"; 
